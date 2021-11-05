4d48dfaddebbbeeb15740509e1a5235630d072cd
Teja7789 <89193972+Teja7789@users.noreply.github.com>
2021-11-05 19:23:53 +0530

    index.html file is added

---
+++ index.js
@@ -0,0 +1,8 @@
+var x = window.prompt("Username").innerText;
+var y = window.prompt("Password").innerText;
+console.log(typeofa)
+console.log(typeofb)
+function f1()
+{
+ window.alert("Details Saved Sucessfully");
+}
